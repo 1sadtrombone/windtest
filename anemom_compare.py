dd1f64a6d9207b27c7985cfdf71b2f8ae4ffedf9
Taj Dyson <taj.dyson@gmail.com>
2020-02-22 21:48:52 -0500

    first comparison plots

--- anemom_compare.py
+++ anemom_compare.py
@@ -1,2 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# get data from an arbitrary day
+data = np.genfromtxt("wind_test_data/20200218.CSV", delimiter=',', skip_header=1)
+
+time = data[:,0]
+anemom1 = data[:,2]
+anemom2 = data[:,5]
+
+plt.plot(anemom1, anemom2, 'k.')
+plt.xlabel("anemom1")
+plt.ylabel("anemom2")
+plt.savefig("plots/first_compare.png")
+plt.clf()
+
+plt.plot(time, anemom1-anemom2, 'k.')
+plt.xlabel("time")
+plt.ylabel("absolute difference")
+plt.savefig("plots/first_diff.png")
