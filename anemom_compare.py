5f11bc3c2c11e3917b495c3b37774f66ec22818f
Taj Dyson <taj.dyson@gmail.com>
2020-02-23 15:25:53 -0500

    anemomcompare now takes advantage of tools.py

--- anemom_compare.py
+++ anemom_compare.py
@@ -1,20 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tools
 
 # get data from an arbitrary day
-data = np.genfromtxt("wind_test_data/20200218.CSV", delimiter=',', skip_header=1)
+data = tools.readDate("20200219")
+
+data = data[data[:,0] < 1582127040] # cut off when anemometers swapped
 
 time = data[:,0]
-anemom1 = data[:,2]
-anemom2 = data[:,5]
+inds = np.argsort(data[:,2])
+anemom1 = data[:,2][inds]
+anemom2 = data[:,5][inds]
+
+p = np.polyfit(anemom1, anemom2, 1)
 
 plt.plot(anemom1, anemom2, 'k.')
+plt.plot(anemom1, p[0]*anemom1+p[1], 'r--')
 plt.xlabel("anemom1")
 plt.ylabel("anemom2")
-plt.savefig("plots/first_compare.png")
-plt.clf()
-
-plt.plot(time, anemom1-anemom2, 'k.')
-plt.xlabel("time")
-plt.ylabel("absolute difference")
-plt.savefig("plots/first_diff.png")
+plt.title(f"$a_2 = {p[0]}*a_1+{p[1]}$")
+plt.savefig("plots/fit_with_no_errors.png")
